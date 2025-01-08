b39bdbd15c3b8d3b203da431229644f0a4776d61
caokhoi2702 <ngcaokhoi2702@gmail.com>
2025-01-09 00:36:50 +0700

    huhu

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -25,6 +25,7 @@ datasets_dict = {
 def get_dataset(name, root):
     cur_dict = datasets_dict[name]
     if name=='imagenet':
+        print(os.path.dirname(os.path.realpath(__file__)))
         dataset = ImageNet("../", split=cur_dict['split'], transform=cur_dict['transform'])
     try:
         file_name = cur_dict['indices_csv']
