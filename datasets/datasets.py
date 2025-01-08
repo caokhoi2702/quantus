fb73df02dbc7b6ddd3169a3419c5b11487392821
caokhoi2702 <ngcaokhoi2702@gmail.com>
2025-01-08 23:26:57 +0700

    change dataset path

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -24,7 +24,7 @@ datasets_dict = {
 def get_dataset(name, root):
     cur_dict = datasets_dict[name]
     if name=='imagenet':
-        dataset_path = "../input/imagenet" #For kaggle only
+        dataset_path = "../../kaggle/input/imagenet" #For kaggle only
         # dataset_path = path.join(root, name)
         dataset = ImageNet(dataset_path, split=cur_dict['split'], transform=cur_dict['transform'])
     try:
